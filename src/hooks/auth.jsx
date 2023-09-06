b2ac49d0f1cadf3d1dd8199bd1bb0de2ff842c5c
Victor Paranhos <victor_paranhos@hotmail.com>
2023-09-06 11:58:45 -0300

    add auth

--- src/hooks/auth.jsx
+++ src/hooks/auth.jsx
@@ -1,14 +1,23 @@
-import { createContext, useContext, useState } from "react";
+import { createContext, useContext, useState, useEffect } from "react";
 import { api } from "../services/api";
 const AuthContext = createContext({})
 
 function AuthProvider({children}){
-    const [data, setData] = useState("")
+    const [data, setData] = useState({})
+    function signOut(){
+        localStorage.removeItem("rocketnotes:@token")
+        localStorage.removeItem("rocketnotes:@user")
+        setData({})
+    }
     async function signIn({email, password}){
 
         try{
             const response = await api.post("/sessions", {email, password})
             const {user, token } = response.data
+
+            localStorage.setItem("rocketnotes:@user", JSON.stringify(user))
+            localStorage.setItem("rocketnotes:@token", token)
+
             api.defaults.headers.authorization = `Bearer ${token}`
             setData({user, token})
         }catch(error){
@@ -21,8 +30,19 @@ function AuthProvider({children}){
         
     }
 
+    useEffect(()=> {
+        const token = localStorage.getItem("rocketnotes:@token")
+        const user = localStorage.getItem("rocketnotes:@user")
+        if(token && user){
+            api.defaults.headers.authorization = `Bearer ${token}`
+            setData({token, user: JSON.parse(user)})
+        }
+
+
+    }, [])
+
     return(
-        <AuthContext.Provider value={{signIn, user: data.user}}>
+        <AuthContext.Provider value={{signOut, signIn, user: data.user}}>
             {children}
         </AuthContext.Provider>
     )
