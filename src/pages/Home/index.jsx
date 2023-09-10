fb28be0a51a0e556d1796e2bb27c34cf75cf5416
Victor Paranhos <victor_paranhos@hotmail.com>
2023-09-10 15:05:02 -0300

    tags

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -6,8 +6,36 @@ import {FiPlus, FiSearch} from 'react-icons/fi'
 import { Input } from '../../componentes/Input';
 import { Section } from '../../componentes/Section';
 import { Note } from '../../componentes/Note';
+import { useState, useEffect } from 'react';
+
+import { api } from '../../services/api';
 
 export function Home(){
+  const [tags, setTags] = useState([])
+  const [tagsSelected, setTagsSelected] = useState([])
+
+  function handleTagsSelected(tagname){
+    const alreadySelected = tagsSelected.includes(tagname)
+    
+    if(alreadySelected){
+      const filteredTagsSelected = tagsSelected.filter(tag=> tag !== tagname)
+      setTagsSelected(filteredTagsSelected)
+    }else {
+      setTagsSelected(prevState => [...prevState, tagname])
+    }
+
+    
+  }
+
+  useEffect(()=> {
+    async function fetchTags(){
+        const response = await api.get("/tags")
+        setTags(response.data)
+    }
+
+    fetchTags()
+  }, [])
+
   return(
     <Container>
       <Brand>
@@ -15,11 +43,10 @@ export function Home(){
       </Brand>
       <Header/>
       <Menu>
-
-       <li><ButtonText title="Todos" isActive /></li>
-       <li><ButtonText title="React" /></li>
-       <li><ButtonText title="Node" /></li>
-
+        <li><ButtonText title="Todos" isActive={tagsSelected.length === 0} /></li>
+        {
+          tags && tags.map(tag=> <li key={tag.id} ><ButtonText isActive={tagsSelected.includes(tag.name)}  onClick={()=> handleTagsSelected(tag.name)} title={tag.name}/></li>)
+        }
       </Menu>
       <Search>
         <Input placeholder="Pesquisar pelo titulo" icon={FiSearch} />
--- src/pages/New/index.jsx
+++ src/pages/New/index.jsx
@@ -5,13 +5,24 @@ import { NoteItem } from '../../componentes/NoteItem';
 import { Section} from '../../componentes/Section';
 import { Button} from '../../componentes/Button';
 import {Container, Form} from './styles';
-import {Link} from 'react-router-dom';
+import {Link, useNavigate} from 'react-router-dom';
 import { useState } from 'react';
+import { api } from '../../services/api';
 
 
 export function New(){
+const navigate = useNavigate()
+const [title, setTitle] = useState("")
+const [description, setDescription] = useState("")
+
 const [links, setLinks] = useState([])
 const [newLink, setNewLinks] = useState("")
+
+const [tags, setTags] = useState([])
+const [newTags, setNewTags] = useState("")
+
+
+
 function handleAddLink(){
   setLinks(prevState => [...prevState, newLink])
   setNewLinks("")
@@ -19,6 +30,37 @@ function handleAddLink(){
 function handleRemoveLink(deleted){
   setLinks(prevState => prevState.filter(link => link !== deleted))
 } 
+function handleAddTag(){
+  setTags(prevState => [...prevState, newTags])
+  setNewTags("")
+}
+function handleRemoveTag(deleted){
+  setTags(prevState => prevState.filter(tag => tag !== deleted))
+} 
+
+async function handleNewNote(){
+  if(!title){
+    return alert("Você precisa digitar um titulo")
+  }
+  if(!description){
+    return alert("Você precisa digitar uma Descrição")
+  }
+  if(newLink){
+    return alert("Você deixou um Link digitado mas não adicionou")
+  }
+  if(newTags){
+    return alert("Você deixou uma Tag digitada mas não adicionou")
+  }
+  await api.post("/notes", {
+    title,
+    description,
+    tags,
+    links
+  })
+
+  alert("Notas criadas com sucesso!!")
+  navigate("/");
+}
 
   return(
     <Container>
@@ -30,8 +72,8 @@ function handleRemoveLink(deleted){
             <h1>Criar Nota</h1>
             <Link to="/">voltar</Link>
           </header>
-          <Input placeholder="Título"/>
-          <Textarea placeholder="Observações"/>
+          <Input onChange={e=> setTitle(e.target.value)} placeholder="Título"/>
+          <Textarea onChange={e=> setDescription(e.target.value)} placeholder="Observações"/>
           <Section title="Links úteis">
             {links.map((link, index )=> <NoteItem key={String(index)} value={link} onClick={()=> handleRemoveLink(link)}/>)}
             <NoteItem isNew value={newLink} onChange={e => setNewLinks(e.target.value)} onClick={handleAddLink} placeholder="Novo Link"/>
@@ -39,12 +81,12 @@ function handleRemoveLink(deleted){
           </Section>
           <Section title="Marcadores">
             <div className="tags" >
-            <NoteItem value="react"/>
-            <NoteItem isNew placeholder="Nova Tag"/>
+            {tags.map((tag, index)=> <NoteItem key={String(index)} value={tag} onClick={()=> handleRemoveTag(tag)}/>)}
+            <NoteItem value={newTags} onChange={e=> setNewTags(e.target.value)} onClick={handleAddTag} isNew placeholder="Nova Tag"/>
             </div>        
           </Section>
 
-          <Button name="Salvar"/>
+          <Button onClick={handleNewNote} name="Salvar"/>
           
         </Form>
       
